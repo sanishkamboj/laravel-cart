46e74efa07f11027d33dc08bb84ea25801a55c76
Sanish <sanish@shinewebservices.com>
2020-01-14 17:06:48 +0530

    initial Commit

---
+++ src/CartServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Devbuddy\Cart;
+
+use Illuminate\Support\ServiceProvider;
+use App;
+class CartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('cart', function()
+		{
+			return new Cart();
+		});
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/facades/CartFacade.php
@@ -0,0 +1,8 @@
+<?php namespace Devbuddy\Cart\Facades;
+use Illuminate\Support\Facades\Facade;
+class CartFacade extends Facade {
+    protected static function getFacadeAccessor()
+    {
+        return 'cart';
+    }
+}
